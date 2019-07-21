47072a30e340a5b92243cf793d178d6a9dc250ee
Mpilo Pillz <mpilopillz@Mpilos-MacBook-Pro.local>
2019-07-21 15:26:54 +0200

    added controller to hard code player guesses. next is to get the guess from the from

--- abattleship.js
+++ abattleship.js
@@ -35,8 +35,7 @@ var model = {
     for (var i = 0; i < this.numShips; i++){
       var ship = this.ships[i];
       // locations = ship.locations;
-      var index = locations.indexOf(guess);
-      ship.locations.indexOf(guess);
+      var index = ship.locations.indexOf(guess);
       if(index >= 0){
         ship.hits[index] = "hit";
         view.displayHit(guess);
@@ -62,6 +61,64 @@ var model = {
     return true;
   }
 };
+
+var controller = {
+  guesses: 0,
+  processGuess : function(guess){
+    var location = parseGuess(guess);
+
+    if(location){
+      this.guesses++;
+      var hit = model.fire(location);
+      if(hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+      }
+    }
+  },
+  
+  
+};
+
+function parseGuess(guess){
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  if(guess === null || guess.length !== 2){
+    alert("Please enter a letter and a number on the board");
+  } else{
+    firstChar = guess.charAt(0);
+    var row = alphabet.indexOf(firstChar);
+    // console.log(row);
+    var column = guess.charAt(1);
+    // console.log(column);
+
+    if(isNaN(row) || isNaN(column)) {
+      alert("That is not on the board")
+    } else if( row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
+      alert("Oops, that falls outside the range of the board");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+controller.processGuess("A0");
+
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
+
+// console.log(parseGuess("A2"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
 
 
 
